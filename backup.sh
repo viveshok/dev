0542646ce7084ad57d9e82b25ae76bc48aa99c5d
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2015-11-01 12:35:54 +0800

    updated backup.sh

--- backup.sh
+++ backup.sh
@@ -7,10 +7,11 @@ set -e
 # sudo fdisk -l # to find nanme /dev/xxx of disk to mount
 sudo mount -t ntfs /dev/sdb1 /media/external
 
-rsync -av --delete /home/abeaulne/ /media/external/backup/ >> /home/abeaulne/utils/backup.log
+rsync -avr --delete /home/abeaulne/data /media/external/
 
 sudo umount /media/external # before you disconnect
 
 # for remote server
+
 rsync -avr --delete /home/abeaulne/data abeaulne@128.199.101.40:/home/abeaulne
 
