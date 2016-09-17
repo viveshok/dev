ce18d365056d1790a2b12ba725aa8e7ffa1f3055
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2016-09-17 14:12:03 +0800

    changed how to install awscli

--- osx.sh
+++ osx.sh
@@ -10,7 +10,9 @@ brew install gnupg
 # Docker
 
 # aws client
-pip install awscli
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 # tmux
 brew install tmux
