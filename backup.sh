6d6c54a49a994e5865709cf7218e7c1af3b4bec8
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2017-04-04 09:38:49 -0400

    added OSX breadcrumb

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@ set -e
 # for external disk
 
 # sudo fdisk -l # to find nanme /dev/xxx of disk to mount
+# diskutil list # same but on OSX
 sudo mount -t ntfs /dev/sdb1 /media/external
 
 rsync -avr --delete /home/abeaulne/data /media/external/
