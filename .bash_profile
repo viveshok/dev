4c8e5ba9cca6b0934a8a4e29521a0248d0e2346a
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2017-05-27 20:46:09 -0500

    debian make vim default editor

--- .bash_profile
+++ .bash_profile
@@ -3,4 +3,5 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 export PS1="\W $ "
+export editor=vim
 
--- debian.sh
+++ debian.sh
@@ -6,6 +6,8 @@ sudo apt-get install git
 sudo apt-get install vim
 sudo apt-get install tmux
 
+echo 'source ~/.bash_profile' >> ~/.bashrc
+
 wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
 bash Anaconda3-4.3.1-Linux-x86_64.sh -b
 echo 'export PATH="/home/abeaulne/anaconda3/bin:$PATH"' >> ~/.bashrc
