6eee5d959854153920851c8f14b8e3b5894002ae
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2017-05-27 20:31:18 -0500

    add anaconda and awscli to setup script

--- debian.sh
+++ debian.sh
@@ -6,3 +6,8 @@ sudo apt-get install git
 sudo apt-get install vim
 sudo apt-get install tmux
 
+wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+bash Anaconda3-4.3.1-Linux-x86_64.sh -b
+echo 'export PATH="/home/abeaulne/anaconda3/bin:$PATH"' >> ~/.bashrc
+
+pip install awscli
