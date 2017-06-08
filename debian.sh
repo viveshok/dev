82139e7605aa80def1178f7091de70514869f4e2
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2017-06-07 19:09:55 -0500

    debian script: add scrot

--- debian.sh
+++ debian.sh
@@ -5,6 +5,7 @@ sudo apt-get install chromium-browser
 sudo apt-get install git
 sudo apt-get install vim
 sudo apt-get install tmux
+sudo apt-get install scrot
 
 echo 'source ~/.bash_profile' >> ~/.bashrc
 
