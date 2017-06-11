6242c2298d60d0379a3e22a3c2f028365dbf9cfd
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2017-06-11 14:32:55 -0500

    add edit option to key manager

--- keymgr.sh
+++ keymgr.sh
@@ -1,9 +1,9 @@
 
 set -e
 
-SYNOPSIS="Synopsis: $ bash keymgr.sh (get|set) KEYWORD"
 VAULT=~/vault.gpg
 AWS_URI="s3://filevault/vault.gpg"
+SYNOPSIS="Synopsis: $ bash keymgr.sh (get|set|edit) [KEYWORD]"
 ACTION=$1
 KEYWORD=$2
 RANDOM_WORD=`awk -v lineno="$RANDOM" 'lineno==NR{print;exit}' /usr/share/dict/words`
@@ -15,22 +15,21 @@ if [ ! -e $VAULT ]; then
     exit -1
 fi
 
-if [ -z $KEYWORD ]; then
-    echo "Keyword required!"
-    echo $SYNOPSIS
-    exit -1
-fi
+function ensure_keyword {
+    if [ -z $1 ]; then
+        echo "Keyword required!"
+        echo $SYNOPSIS
+        exit -1
+    fi
+}
 
 read -sp "Enter vault's password: " PASSWORD; echo
 
 if [ $ACTION == 'get' ]; then
+    ensure_keyword $KEYWORD
     gpg --passphrase $PASSWORD --batch --yes --decrypt $VAULT | grep -i $KEYWORD
 elif [ $ACTION == 'set' ]; then
-    read -sp "Confirm vault's password: " PASSWORD2; echo
-    if [ ! $PASSWORD == $PASSWORD2 ]; then
-        echo "Passwords don't match!!!"
-        exit -1
-    fi
+    ensure_keyword $KEYWORD
     read -p 'Enter key (or leave empty to generate one): ' KEY; echo
     if [ -z $KEY ]; then
         KEY=`< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c25`
@@ -38,6 +37,13 @@ elif [ $ACTION == 'set' ]; then
     gpg --passphrase $PASSWORD --batch --yes --decrypt $VAULT > $CACHE_FILE
     MSG="Key for '$KEYWORD' is '$KEY'"
     echo $MSG | tee --append $CACHE_FILE
+    gpg --passphrase $PASSWORD --batch --yes --symmetric --output $VAULT $CACHE_FILE
+    rm --verbose $CACHE_FILE
+    rm --verbose --force .*.tmp
+    aws s3 cp $VAULT $AWS_URI
+elif [ $ACTION == 'edit' ]; then
+    gpg --passphrase $PASSWORD --batch --yes --decrypt $VAULT > $CACHE_FILE
+    vim $CACHE_FILE
     gpg --passphrase $PASSWORD --batch --yes --symmetric --output $VAULT $CACHE_FILE
     rm --verbose $CACHE_FILE
     rm --verbose --force .*.tmp
