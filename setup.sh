c9a556e610976c0909d93bc4697fdc52356d0278
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2018-04-26 22:04:14 -0400

    clean up setup script

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 # set blue background
 xsetroot -solid midnightblue
 
-## set up external monitor
 #xrandr --newmode "1920x1080_60"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 #xrandr --delmode HDMI-0 1920x1080_60
 #xrandr --addmode HDMI-0 1920x1080_60
 #xrandr --auto --output HDMI-0 --mode 1920x1080_60 --right-of eDP
+#xrandr --output HDMI-0 --mode 1920x1081 --rate 60 --right-of eDP
+#xrandr --output DisplayPort-0 --mode 2560x1440 --rate 60 --left-of eDP
+xrandr --auto --output DisplayPort-0 --mode 2560x1440 --rate 60 --right-of eDP
 
-# set up external monitor
-xrandr --output DisplayPort-0 --mode 2560x1440 --rate 60 --left-of eDP
