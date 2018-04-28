c498bffd7e991babc1655598608b288de2617c7c
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2018-04-28 10:54:10 -0400

    get dual monitor setup right

--- setup.sh
+++ setup.sh
@@ -8,6 +8,5 @@ xsetroot -solid midnightblue
 #xrandr --addmode HDMI-0 1920x1080_60
 #xrandr --auto --output HDMI-0 --mode 1920x1080_60 --right-of eDP
 #xrandr --output HDMI-0 --mode 1920x1081 --rate 60 --right-of eDP
-#xrandr --output DisplayPort-0 --mode 2560x1440 --rate 60 --left-of eDP
-xrandr --auto --output DisplayPort-0 --mode 2560x1440 --rate 60 --right-of eDP
+xrandr --output DisplayPort-0 --mode 2560x1440 --rate 60 --right-of eDP
 
