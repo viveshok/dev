d22b60cca1472489c921dd60e6e3dd8312322739
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2018-10-25 23:25:00 -0400

    edited setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,6 @@
 # set blue background
 xsetroot -solid midnightblue
 
-#xrandr --newmode "1920x1080_60"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
-#xrandr --delmode HDMI-0 1920x1080_60
-#xrandr --addmode HDMI-0 1920x1080_60
-#xrandr --auto --output HDMI-0 --mode 1920x1080_60 --right-of eDP
-#xrandr --output HDMI-0 --mode 1920x1081 --rate 60 --right-of eDP
-xrandr --output DisplayPort-0 --mode 2560x1440 --rate 60 --right-of eDP
+# reduce macbook screen resolution
+xrandr --output eDP --mode "1920x1200"
 
