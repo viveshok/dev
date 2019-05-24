a515c388819ab01a09266eedc38b898caddc2d61
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2019-05-24 04:52:50 -0400

    grep with undesirable filtered

---
+++ grep.sh
@@ -0,0 +1,11 @@
+
+set -e
+
+grep -Iinr $1 $2 \
+    | grep -v mypy_cache \
+    | grep -v '\.pytest_cache' \
+    | grep -v '\.ipynb' \
+    | grep -v '\.svg' \
+    | grep -v '\.git' \
+    | grep -v '\.html'
+
