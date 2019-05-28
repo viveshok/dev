dc72fb1dc8886803a24aa4b88da6cd436edafa82
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2019-05-28 09:06:34 -0400

    grep exclude html files

--- grep.sh
+++ grep.sh
@@ -2,10 +2,10 @@
 set -e
 
 grep -Iinr $1 $2 \
-    | grep -v mypy_cache \
+    | grep -v 'mypy_cache' \
     | grep -v '\.pytest_cache' \
     | grep -v '\.ipynb' \
     | grep -v '\.svg' \
     | grep -v '\.git' \
-    | grep -v '\.html'
+    | grep -v '\.html:'
 
