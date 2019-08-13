ccd8e9d08b6d49053f93fa7e42ee7b2da224529e
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2019-08-13 09:13:41 -0400

    XMonad: add F1 to F12 as workspaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,9 +8,10 @@ import XMonad.Util.EZConfig (additionalKeys)
 -- mod4Mask -> (command on mac)
 myModMask = mod1Mask
 
-myExtraWorkspaces = [(xK_0, "0"), (xK_minus, "-"), (xK_equal, "=")]
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] ++ (map snd myExtraWorkspaces)
+myExtraWorkspaces = [(xK_F1, "F1"), (xK_F2, "F2"), (xK_F3, "F3"), (xK_F4, "F4"), (xK_F5, "F5"), (xK_F6, "F6"),
+                     (xK_F7, "F7"), (xK_F8, "F8"), (xK_F9, "F9"), (xK_F10, "F10"), (xK_F11, "F11"), (xK_F12, "F12")]
 
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] ++ (map snd myExtraWorkspaces)
 
 myAdditionalKeys =
     [
