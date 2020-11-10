1cd12cc7cc41bfd198e588be9a9a5e0f289916cc
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2020-11-10 22:20:18 +0400

    filter out pytype from grep

--- grep.sh
+++ grep.sh
@@ -5,6 +5,7 @@ grep -Iinr $1 $2 \
     | grep -v 'mypy_cache' \
     | grep -v '\.pytest_cache' \
     | grep -v '\.ipynb' \
+    | grep -v '\.pytype' \
     | grep -v '\.svg' \
     | grep -v '\.git' \
     | grep -v '\.html:'
